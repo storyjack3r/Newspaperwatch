4e58c3d4b8b2e3fea60d06df3ca665fed98dd8c2
System <none>
2017-05-10 20:30:33 +0000

    🌷🚛 Updated with Glitch 2

--- server.js
+++ server.js
@@ -20,20 +20,14 @@ app.use(express.static('public'));
 
 /* You can use uptimerobot.com or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 
-app.all("/" + process.env.BOT_ENDPOINT, function (request, response) {
-    /* The example below tweets out "Hello world!". */
-    retweetLatest();
-  
-  
-  
-  
-/* The example below tweets out "Hello world!". */
-  
-  
-  
-  
+var successVar=0;
 
+app.all("/" + process.env.BOT_ENDPOINT, function (request, response) {
   
+    retweetLatest();
+  if (successVar=0) {
+    response
+  }
   
 });
 
@@ -45,7 +39,7 @@ var listener = app.listen(process.env.PORT, function () {
 //---------------------------
 
 var trumpCounter = {search:'Trump', counter: 0};
-var brexitCounter = {search:'Brexit', counter: 0};
+var farronCounter = {search:'Farron', counter: 0};
 var terrorismCounter = {search:'terror', counter: 0};
 var electionCounter = {search:'election', counter:0};
 var sportCounter = {search:/sport/i, counter:0};
@@ -56,7 +50,7 @@ var statusComposer;
 function orderFacts(){
   
   statsList[0]= trumpCounter.counter;
-   statsList[1]=brexitCounter.counter;
+   statsList[1]=farronCounter.counter;
    statsList[2]=terrorismCounter.counter;
    statsList[3]=electionCounter.counter;
    statsList[4]=sportCounter.counter;
@@ -73,8 +67,8 @@ function orderFacts(){
               nextStat = term + "mentions: " + i + "%";
               break;
 
-          case brexitCounter.counter:
-              term = brexitCounter.search;
+          case farronCounter.counter:
+              term = farronCounter.search;
               nextStat = term + "mentions: " + i + "%";
               break;
 
@@ -142,13 +136,9 @@ function retweetLatest() {
 
 T.get('statuses/user_timeline', options1 , function (error, data, response) {
   
-	  if (!error) {
-    	//random selection method
-      //var x=Math.floor((Math.random() * countN)+1);
-      //guardianRtTxt = data[x].text;
-      
+	  if (!error) {      
       searchForSubject(data, trumpCounter);
-      searchForSubject(data, brexitCounter);
+      searchForSubject(data, farronCounter);
           searchForSubject(data, electionCounter);
           searchForSubject(data, terrorismCounter);
       searchForSubject(data, sportCounter);
@@ -158,12 +148,9 @@ T.get('statuses/user_timeline', options1 , function (error, data, response) {
       Tw.get('statuses/user_timeline', options2 , function (err, dat, resp) {
         
 	      if (!err) {
-        
-          // var y=Math.floor((Math.random() * 10)+1);
-          // torygraphRtTxt = dat[y].text;
           
           searchForSubject(dat, trumpCounter);
-      searchForSubject(dat, brexitCounter);
+      searchForSubject(dat, farronCounter);
           searchForSubject(dat, electionCounter);
           searchForSubject(dat, terrorismCounter);
       searchForSubject(dat, sportCounter);
@@ -173,115 +160,36 @@ T.get('statuses/user_timeline', options1 , function (error, data, response) {
           
           
           
-  var retweetTxt = "📰 May mentions: " + mayCounter.counter + "% - Corbyn mentions: " + corbynCounter.counter + "% - 🍦 Trump mentions: " + trumpCounter.counter +"% - Brexit 🚪 mentions: " + brexitCounter.counter +"% - Sport 🏆 mentions: " + sportCounter.counter +"%";
+          
+  var retweetTxt = "May mentions: " + mayCounter.counter + "% | Corbyn mentions: " + corbynCounter.counter + "% | Farron mentions: " + farronCounter.counter +"% | Trump 🍦 mentions: " + trumpCounter.counter +"% | Sport 🏆 mentions: " + sportCounter.counter +"%";
       
   statusComposer = retweetTxt;
   
 		
 	T.post('statuses/update', { status: statusComposer}, function(error, data, response) {
-			if (response) {
+		
+    if (response) {
         response.sendStatus(200);
           
           
         }
-        resp.sendStatus(200);
-      });
-      
-// var retweetTxt = guardianRtTxt + torygraphRtTxt;
-      
-//       // //take out @mentions
-//       // retweetTxt=retweetTxt.replace(/(@\S+)/gi,"");
-//       // //remove hashtags
-//       // retweetTxt=retweetTxt.replace("#","");
-//       // //take out retweets
-//       // retweetTxt=retweetTxt.replace("RT:","");
-//       // retweetTxt=retweetTxt.replace("RT","");
-//       // //take out additional spaces
-//       // retweetTxt=retweetTxt.replace(/\s+/g,' ').trim();
-//       // var rtTxtArray =retweetTxt.split(" ");
-//       // // var retweetTxt = wordSwapsies(rtTxtArray);
-//       // retweetTxt=retweetTxt.replace("undefined","");
-//       // var tweeter = data.statuses[x].user.name;
-      
-//        // var statusComposer = "'" + retweetTxt + "' - " + tweeter;
-//   var statusComposer = retweetTxt;
-//       //var statusComposer = "Hello James!";
-		
-// 	T.post('statuses/update', { status: statusComposer}, function(error, data, response) {
-// 			if (response) {
-//         response.sendStatus(200);
-         
         
+        });
 			}
 			// If there was an error with our Twitter call, we print it out here.
 			if (error) {
 				
         response.sendStatus(500);
-        var statusComposer = "500 error";
+    
 			}
 		})
 	  }
-	  // However, if our original search request had an error, we want to print it out here.
+	
 	  else {
-	  	console.log('There was an error with your search:', error);
+	  	
       response.sendStatus(200);
       
 	  }
+
 	});
 }
-//   function wordSwapsies(txtArray){
-//   for (var i = 0; i < txtArray.length; i++) {
-// 	var thisWord = txtArray[i];
-// 	var changeWord;
-//   var newAdvice;
-// switch (thisWord) {
-//     case "good":
-//         changeWord = "bad";
-//         break;
-//     case "bad":
-//         changeWord = "good";
-//         break;
-//     case "love":
-//         changeWord = "hate";
-//         break;
-//     case "hate":
-//         changeWord = "love";
-//         break;
-//     case "ignore":
-//         changeWord = "listen to";
-//         break;
-//     case "best":
-//         changeWord = "worst";
-//         break;
-//     case "worst":
-//         changeWord = "best";
-//         break;
-//     case "friends":
-//         changeWord = "enemies";
-//         break;
-//     case "family":
-//         changeWord = "strangers";
-//         break;
-//     case "enemies":
-//         changeWord = "friends";
-//         break;
-//     case "philosophy":
-//         changeWord = "bullshit";
-//         break;
-//     case "right":
-//         changeWord = "wrong";
-//         break;
-//     case "helpful":
-//         changeWord = "useless";
-//         break;
-//     default:
-//     changeWord = thisWord;
-//     }
-
-// newAdvice+= changeWord + " ";
-
-// }
-
-//     return newAdvice;
-    
-//   }
